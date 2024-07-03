8a36ff608988590f26240e9977f69509c1532e20
HanJin Choi <156498561+halfoffaceeyes@users.noreply.github.com>
2024-07-03 17:48:55 +0900

    Title: 달에서 무게 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240703/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int w=13;
+    float ratio = 0.165;
+    cout << fixed;
+    cout.precision(6);
+    cout << w << " * " << ratio << " = "<< w*ratio<<endl;
+    return 0;
+}

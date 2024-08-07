f19462d665afeac47f0bead258d3f5e3ab265adc
HanJin Choi <156498561+halfoffaceeyes@users.noreply.github.com>
2024-08-07 19:50:14 +0900

    Title: 데이터 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240807/데이터 교환/exchange-data.cpp
+++ 240807/데이터 교환/exchange-data.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int a=5, b=6, c=7,temp;
-    temp=a;
+    int a=5, b=6, c=7,temp,temp2;
+    temp=a,temp2=b;
     a=c;
     c=b;
-    b=a;
+    b=temp;
     cout << a<<endl;
     cout << b<<endl;
     cout << c<<endl;

98b4f90527b39760735b4585cecacbd2bf9baadb
HanJin Choi <156498561+Hanjin-Choi@users.noreply.github.com>
2024-08-26 22:05:33 +0900

    Title: 날짜 변경하여 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240826/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
+++ 240826/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int d,m,y;
-    cin>>d;
-    cin.get();
     cin>>m;
+    cin.get();
+    cin>>d;
     cin.get();
     cin >> y;
     cout <<y<<'.'<<m<<'.'<<d;

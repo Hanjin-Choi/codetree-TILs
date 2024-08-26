d6b746ab70b1ae5ed478d2044645fa16c9559063
HanJin Choi <156498561+Hanjin-Choi@users.noreply.github.com>
2024-08-26 19:34:13 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240826/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    float a=0,b=0,c=0;
+    cin >>a>>b>>c;
+    cout<<fixed;
+    cout.precision(3);
+    cout << a<<endl;
+    cout << b<<endl;
+    cout << c<<endl;
+    return 0;
+}

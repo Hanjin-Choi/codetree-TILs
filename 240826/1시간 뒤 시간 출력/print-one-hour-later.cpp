6377cc0191a49dd3cedddcbdc3e0a861d94fe12f
HanJin Choi <156498561+Hanjin-Choi@users.noreply.github.com>
2024-08-26 21:29:15 +0900

    Title: 1시간 뒤 시간 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240826/1시간 뒤 시간 출력/print-one-hour-later.cpp
+++ 240826/1시간 뒤 시간 출력/print-one-hour-later.cpp
@@ -4,8 +4,9 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int a,b;
-    char c;
-    cin>>a>>c>>b;
-    cout<<a<<endl<<b;
+    cin>>a;
+    cin.get();
+    cin>>b;
+    cout<<a+1<<':'<<b;
     return 0;
 }

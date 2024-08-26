dad7a86b2900346be1edd0d753f49365b1814054
HanJin Choi <156498561+Hanjin-Choi@users.noreply.github.com>
2024-08-26 22:04:40 +0900

    Title: 날짜 변경하여 출력 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240826/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int d,m,y;
+    cin>>d;
+    cin.get();
+    cin>>m;
+    cin.get();
+    cin >> y;
+    cout <<y<<'.'<<m<<'.'<<d;
+    return 0;
+}

305699c41fe196a8773d451d378bc4d77d09a57e
HanJin Choi <156498561+Hanjin-Choi@users.noreply.github.com>
2024-08-26 20:26:03 +0900

    Title: 실수와 문자 받아 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240826/실수와 문자 받아 출력하기/print-number--and-letter.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char c;
+    float a,b;
+    cin >>c>>a>>b;
+    cout<<fixed;
+    cout.precision(2);
+    cout << c<<endl;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    return 0;
+}

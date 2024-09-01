190799cc5f2e4f6f589d1cb468aa701af1b98774
HanJin Choi <156498561+Hanjin-Choi@users.noreply.github.com>
2024-09-01 18:58:01 +0900

    Title: 1부터 특정 수까지의 합, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240901/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int sum(int n)
+{
+    int res=0;
+    for (i=1;i<=n;i++)
+    {
+        res+=i
+    }
+    return res
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin>>n;
+    cout << sum(n) /10;
+    return 0;
+}

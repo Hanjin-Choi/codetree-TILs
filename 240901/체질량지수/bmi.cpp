68b0c11141344e9be256be36c782c735da39f55b
HanJin Choi <156498561+Hanjin-Choi@users.noreply.github.com>
2024-09-01 18:22:47 +0900

    Title: 체질량지수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240901/체질량지수/bmi.cpp
+++ 240901/체질량지수/bmi.cpp
@@ -3,13 +3,10 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int h,w;
-    double b;
+    int h,w,b;
     cin >> h>>w;
-    b=(double)10000*w/h/h;
+    b=10000*w/h/h;
     
-    cout <<fixed;
-    cout.precision(2);
     cout<<b<<endl;
 
     if(b>=25)cout<<"Obesity";

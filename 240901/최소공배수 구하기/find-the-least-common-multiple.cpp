1b9ca3198b88bd8cd320fcf7a5a82d58aae31289
HanJin Choi <156498561+Hanjin-Choi@users.noreply.github.com>
2024-09-01 18:56:02 +0900

    Title: 최소공배수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240901/최소공배수 구하기/find-the-least-common-multiple.cpp
+++ 240901/최소공배수 구하기/find-the-least-common-multiple.cpp
@@ -9,13 +9,16 @@ int gcd(int n, int m)
         m=n;
         n=temp;
     } 
-    if(n%m==0) return m;
-    else return gcd(m,n%m);
+    if(n%m==0) 
+        return m;
+    else 
+        return gcd(m,n%m);
 }
 
 int main() {
     // 여기에 코드를 작성해주세요.
     int n,m;
+    cin>>n>>m;
     cout << n*m/gcd(n,m);
     return 0;
 }

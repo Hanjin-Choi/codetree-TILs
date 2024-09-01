b04c9b7c71988171f395c6507ae30f585b0180dd
HanJin Choi <156498561+Hanjin-Choi@users.noreply.github.com>
2024-09-02 00:52:02 +0900

    Title: 사칙연산 함수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240902/사칙연산 함수/quadratic-operations-function.cpp
+++ 240902/사칙연산 함수/quadratic-operations-function.cpp
@@ -3,17 +3,17 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int a,c;
+    int a=0,c=0;
     char o;
     cin >>a >>o>>c;
     if (o =='+')
-        cout << a << ' '<< o<<' '<< c << ' = '<<a+c;
+        cout << a << ' '<< o<<' '<< c << " = "<<a+c;
     else if (o =='-')
-        cout << a << ' '<< o<<' '<< c << ' = '<<a-c;
+        cout << a << ' '<< o<<' '<< c << " = "<<a-c;
     else if (o =='/')
-        cout << a << ' '<< o<<' '<< c << ' = '<<a/c;
+        cout << a << ' '<< o<<' '<< c << " = "<<a/c;
     else if (o =='*')
-        cout << a << ' '<< o<<' '<< c << ' = '<<a*c;  
+        cout << a << ' '<< o<<' '<< c << " = "<<a*c;  
     else
         cout <<"False";
     return 0;
